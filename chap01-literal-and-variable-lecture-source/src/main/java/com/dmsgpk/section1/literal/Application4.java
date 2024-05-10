3968cfb91c87568e40ae20ad95028636ce4c7847
dmsgpk237 <a01027947353@gmail.com>
2024-05-10 17:36:48 +0900

    푸시하고 올리는 거 연습하는중

---
+++ chap01-literal-and-variable-lecture-source/src/main/java/com/dmsgpk/section1/literal/Application4.java
@@ -0,0 +1,12 @@
+package com.dmsgpk.section1.literal;
+
+public class Application4 {
+
+    public static void main(String[] args) {
+
+        System.out.print("hello world");
+        System.out.print("아 배고프다...졸리다");
+
+
+    }
+}
