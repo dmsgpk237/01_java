b5a78b644e93aacae08278e613b2229f64f036f0
DESKTOP-RTFT54O\80405 <a01027947353@gmail.com>
2024-05-14 17:45:14 +0900

    [0514 수업] 1. 비교연산자 2. 논리연산자 3. 논리연산자의 우선순위와 활용 4. and 연산과 or 연산의 특징 5. 삼항연산자

---
+++ chap02-operator-lecture-source/src/main/java/com/dmsgpk/section05/logical/Application3.java
@@ -0,0 +1,21 @@
+package com.dmsgpk.section05.logical;
+
+public class Application3 {
+
+    public static void main(String[] args) {
+
+
+        /*
+        * [AND 연산과 OR 연산의 특징]
+        * 논리식1 && 논리식 2 : 논리식1의 결과가 false이면, 논리식 2를 실행 안 함
+        * -> 논리식1의 위치에 false가 나올 확률이 높은 조건을 작성하는 것이 좋다.
+        *
+        * 논리식1 || 논리식2 : 논리식1의 결과가 true이면, 논리식 2를 실행 안 함
+        * -> 논리식1의 위치에 true가 나올 가능성을 높은 조건을 작성하는 것이 좋다.
+        */
+
+
+
+
+    }
+}
